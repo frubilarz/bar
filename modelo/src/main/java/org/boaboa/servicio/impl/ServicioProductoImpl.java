a6684541d7523f4d9119d5f0457d0196898b233c
Fernando Rubilar <frubilar@icci.cl>
2016-07-19 17:21:12 -0400

    servicio producto
    Signed-off-by: Fernando Rubilar <frubilar@icci.cl>

--- modelo/src/main/java/org/boaboa/servicio/impl/ServicioProductoImpl.java
+++ modelo/src/main/java/org/boaboa/servicio/impl/ServicioProductoImpl.java
@@ -3,81 +3,217 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.boaboa.servicio.impl;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
 import org.boaboa.modelo.Producto;
 import org.boaboa.modelo.TipoProducto;
+import org.boaboa.repository.ProductoRepository;
+import org.boaboa.repository.TipoProductoRepository;
 import org.boaboa.servicio.ServicioProducto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author frubilar
  */
 @Service("servicioProducto")
-public class ServicioProductoImpl implements Serializable,ServicioProducto{
+public class ServicioProductoImpl implements Serializable, ServicioProducto {
+
+    private static final long serialVersionUID = 3533130047836877824L;
+    private static final Logger logger = LoggerFactory.getLogger(ServicioProductoImpl.class);
+
+    @Resource(name = "productoRepository")
+    private ProductoRepository productoRepository;
+
+    @Resource(name = "tipoProductoRepository")
+    private TipoProductoRepository tipoProductoRepository;
 
     @Override
+    @Transactional
     public Producto guardar(Producto producto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Producto salida = null;
+        try {
+            if (producto != null) {
+                salida = productoRepository.save(producto);
+                logger.info("Producto persistido con id: {}", salida.getId());
+            }
+        } catch (Exception e) {
+            salida = null;
+            logger.debug("Error al intentar persistir producto: {}", e.toString(), e);
+            logger.error("Error al intentar persistir producto: {}", e.toString());
+        }
+        return salida;
     }
 
     @Override
+    @Transactional
     public boolean eliminar(Producto producto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean salida = false;
+        try {
+            if (producto != null) {
+                productoRepository.delete(producto);
+                salida = true;
+            }
+        } catch (Exception e) {
+            salida = false;
+            logger.error("Error al intentar eliminar producto: {}", e.toString());
+            logger.debug("Error al intentar eliminar producto: {}", e.toString(), e);
+        }
+        return salida;
     }
 
     @Override
     public Producto consultarProducto(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Producto salida = null;
+        try {
+            if (id != null) {
+                salida = productoRepository.findOne(id);
+            }
+        } catch (Exception e) {
+            salida = null;
+            logger.error("Error al intentar obtener un producto por id: {}", e.toString());
+            logger.debug("Error al intentar obtener un producto por id: {}", e.toString(), e);
+        }
+        return salida;
     }
 
     @Override
     public List<Producto> consultarProductos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Producto> productos = new ArrayList<Producto>();
+        try {
+            productos = productoRepository.findAll();
+        } catch (Exception e) {
+            productos = new ArrayList<Producto>();
+            logger.debug("Error al intentar obtener todos los productos: {}", e.toString(), e);
+            logger.error("Error al intentar obtener todos los productos: {}", e.toString());
+        }
+        return productos;
     }
 
     @Override
     public Producto consultarProducto(String nombre) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Producto producto = null;
+        try {
+            if (StringUtils.isNotEmpty((nombre))) {
+                producto = productoRepository.findByNombreIgnoreCase(nombre);
+            }
+        } catch (Exception e) {
+            producto = null;
+            logger.error("Error al intentar obtener un producto por nombre: {}", e.toString());
+            logger.debug("Error al intentar obtener un producto por nombre: {}", e.toString(), e);
+        }
+        return producto;
     }
 
     @Override
     public List<Producto> consultarProductos(TipoProducto tipoProducto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Producto> productos = new ArrayList<Producto>();
+        try {
+            if (tipoProducto != null) {
+                productos = productoRepository.findByTipoProducto(tipoProducto);
+            }
+        } catch (Exception e) {
+            productos = new ArrayList<Producto>();
+            logger.error("Error al intentar obtener productos por tipo: {}", e.toString());
+            logger.debug("Error al intentar obtener productos por tipo: {}", e.toString(), e);
+        }
+        return productos;
     }
 
     @Override
     public List<Producto> consutlarProductos(boolean stock) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Producto> productos = new ArrayList<Producto>();
+        try {
+            productos = productoRepository.findByStock(stock);
+        } catch (Exception e) {
+            productos = new ArrayList<Producto>();
+            logger.debug("Error al intentar obtener productos por stock: {}", e.toString(), e);
+            logger.error("Error al intentar obtener productos por stock: {}", e.toString());
+        }
+        return productos;
     }
 
     @Override
     public TipoProducto guardar(TipoProducto tipoProducto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TipoProducto salida = null;
+        try {
+            if (tipoProducto != null) {
+                salida = tipoProductoRepository.save(tipoProducto);
+                logger.info("Tipo producto persistido con exito : {}", salida.getId());
+            }
+        } catch (Exception e) {
+            salida = null;
+            logger.error("Error al intentar persistir un tipo producto: {}", e.toString());
+            logger.debug("Error al intentar persistir un tipo producto: {}", e.toString(), e);
+        }
+        return salida;
     }
 
     @Override
     public boolean eliminar(TipoProducto tipoProducto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean salida = false;
+        try {
+            if (tipoProducto != null) {
+                tipoProductoRepository.delete(tipoProducto);
+                salida = true;
+            }
+        } catch (Exception e) {
+            salida = true;
+            logger.debug("Error al intentar eliminar un  tipo producto :{} ", e.toString(), e);
+            logger.error("Error al intentar eliminar un  tipo producto :{} ", e.toString());
+        }
+        return salida;
     }
 
     @Override
     public List<TipoProducto> consultarTiposProductos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<TipoProducto> tipoProductos = new ArrayList<TipoProducto>();
+        try {
+            tipoProductos = tipoProductoRepository.findAll();
+        } catch (Exception e) {
+            tipoProductos= new ArrayList<TipoProducto>();
+            logger.error("Error al intentar consultar toos tipos producto: {}",e.toString());
+            logger.debug("Error al intentar consultar toos tipos producto: {}",e.toString(),e);
+        }
+        return tipoProductos;
     }
 
     @Override
     public TipoProducto consultarTipoProducto(String nombre) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TipoProducto salida = null;
+        try {
+            if(StringUtils.isNotEmpty(nombre)){
+                salida = tipoProductoRepository.findByNombreIgnoreCase(nombre);
+            }
+        } catch (Exception e) {
+            salida = null;
+            logger.error("Error al intentar obtener un tipo producto por nombre: {}",e.toString());
+            logger.debug("Error al intentar obtener un tipo producto por nombre: {}",e.toString(),e);
+        }
+        return salida;
     }
 
     @Override
     public TipoProducto consultarTipoProducto(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TipoProducto salida = null;
+        try {
+            if(id!=null){
+                salida = tipoProductoRepository.findOne(id);
+            }
+        } catch (Exception e) {
+            salida  = null;
+            logger.error("Error al intentar obtener un tipo producto por id: {}",e.toString());
+            logger.debug("Error al intentar obtener un tipo producto por id: {}",e.toString(),e);
+        }
+        return salida;
     }
-    
+
 }
